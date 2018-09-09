112636d9892fedf21c2472f203a4f1ef65aeeca4
Jack <Jack@allblab.com>
2018-09-09 21:45:12 +0900

    first commit

---
+++ src/view/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      {{counter}}
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "Home",
+      data: function () {
+        return {
+          counter: 0,
+
+        }
+      }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
